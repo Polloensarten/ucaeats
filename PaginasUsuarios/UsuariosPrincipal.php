ae4982564b759022da5f3d1a7af62ff1221a4df3
Darkristhgn <cg019912@gmail.com>
2025-11-10 18:07:38 -0800

    Pagina de Usuarios(sin diseño)

---
+++ PaginasUsuarios/UsuariosPrincipal.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>UsuariosPrincipal</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+    <body>
+       <nav class="navbar bg-info border-bottom border-body sticky-top">
+            <div name="BarraSuperior" style="text-align: center;" class="container-fluid">
+                <p class="navbar-brand mb-0 h1">Bienvenido nombreGenerico1</p>
+                <div class="d-flex">
+                    <button class="btn btn-primary"><i class="fa-solid fa-cart-shopping"></i></button>
+                </div>
+            </div>
+       </nav>
+        <div style="display: grid; grid-template-columns: 300px 300px; gap: 40px; grid-template-rows:300px 300px; padding:10px; place-self: center;">
+            <div name="Negocio 1" style="border: 1px solid black; border-radius: 5px;" ><a href="#"><p style="position: absolute; align-self: center; padding-left:135px; color:black">Tacos</p><img src="./Images/Tacos.png" style="width:100%; height:100%; object-fit: cover; border-radius: 5px;" alt="no hay imagen"></a></div>
+            <div name="Negocio 2" style="border: 1px solid black; border-radius: 5px;" ><a href="#"><img src="./Images/placeholder.png" style="width:100%; height:100%; object-fit: cover;"></a></div>
+            <div name="Negocio 3" style="border: 1px solid black; border-radius: 5px;"><a href="#"><img src="./Images/placeholder.png" style="width:100%; height:100%; object-fit: cover;"></a></div>
+            <div name="Negocio 4" style="border: 1px solid black; border-radius: 5px;"><a href="#"><img src="./Images/placeholder.png" style="width:100%; height:100%; object-fit: cover;"></a></div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/a8d9f3784b.js" crossorigin="anonymous"></script>
+    </body>
+</html>
