a35ed201bfd9e22a94a8effccf6b8cdf55444c17
Darkristhgn <cg019912@gmail.com>
2025-11-13 22:38:58 -0800

    Avance de la pagina de  las tiendas

---
+++ PaginasUsuarios/NegocioRand.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>NegocioRand</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="Negocios.css">
+    </head>
+    <body>
+       <nav class="navbar bg-info border-bottom border-body sticky-top">
+            <div name="BarraSuperior" style="text-align: center;" class="container-fluid">
+                <div class="d-flex">
+                    <a href="UsuariosPrincipal.php"><button class="btn btn-danger"><i class="fa-solid fa-arrow-left"></i></button></a>
+                </div>
+                <div class="d-flex">
+                    <button class="btn btn-primary"><i class="fa-solid fa-cart-shopping"></i></button>
+                </div>
+            </div>
+       </nav>
+       <div class="contenedorPrincipal" style="display: grid; grid-template-columns: auto; place-self: center; place-items:center">
+            <div class="contenedor-desplazable">
+                 <div class="Item" style="display: grid;  grid-template-rows:100px,20px;" >
+                    <div class="FoodCard" style="display: flex;">
+                        <div class="FoodImage"style="width:150px; height:150px;  ">
+                            <img src="./Images/placeholder.png" style="width:100%; height:100%; object-fit: cover;">
+                        </div>
+                        <div class="FoodDescription">
+                            <h2>Wario en salsa verde</h2>
+                            <p><i>$25.80</i></p>
+                            <p>Item de comida blablbabla prueba texto para ver si jala lo que estpy haciendo aqui no ocupo mucho texto xd balatro</p>
+                        </div>
+                        
+                    </div>
+                    <div class="FoodBtns" style="display:flex">
+                        <button>Ordenar</button>
+                            <button class="Mas">+</button>
+                                <p>Agregar</p>
+                            <button class="Menos">-</button>
+                    </div>
+                </div>
+                <div class="Item" style="display: grid;  grid-template-rows:100px,20px;" >
+                    <div class="FoodCard" style="display: flex;">
+                        <div class="FoodImage"style="width:150px; height:150px;  ">
+                            <img src="./Images/placeholder.png" style="width:100%; height:100%; object-fit: cover;">
+                        </div>
+                        <div class="FoodDescription">
+                            <h2>Wario en salsa verde</h2>
+                            <p><i>$25.80</i></p>
+                            <p>Item de comida blablbabla prueba texto para ver si jala lo que estpy haciendo aqui no ocupo mucho texto xd balatro</p>
+                        </div>
+                        
+                    </div>
+                    <div class="FoodBtns" style="display:flex">
+                        <button>Ordenar</button>
+                            <button class="Mas">+</button>
+                                <p>Agregar</p>
+                            <button class="Menos">-</button>
+                    </div>
+                </div>
+                <div class="Item" style="display: grid;  grid-template-rows:100px,20px;" >
+                    <div class="FoodCard" style="display: flex;">
+                        <div class="FoodImage"style="width:150px; height:150px;  ">
+                            <img src="./Images/placeholder.png" style="width:100%; height:100%; object-fit: cover;">
+                        </div>
+                        <div class="FoodDescription">
+                            <h2>Wario en salsa verde</h2>
+                            <p><i>$25.80</i></p>
+                            <p>Item de comida blablbabla prueba texto para ver si jala lo que estpy haciendo aqui no ocupo mucho texto xd balatro</p>
+                        </div>
+                        
+                    </div>
+                    <div class="FoodBtns" style="display:flex">
+                        <button>Ordenar</button>
+                            <button class="Mas">+</button>
+                                <p>Agregar</p>
+                            <button class="Menos">-</button>
+                    </div>
+                </div>
+                <div class="Item" style="display: grid;  grid-template-rows:100px,20px;" >
+                    <div class="FoodCard" style="display: flex;">
+                        <div class="FoodImage"style="width:150px; height:150px;  ">
+                            <img src="./Images/placeholder.png" style="width:100%; height:100%; object-fit: cover;">
+                        </div>
+                        <div class="FoodDescription">
+                            <h2>Wario en salsa verde</h2>
+                            <p><i>$25.80</i></p>
+                            <p>Item de comida blablbabla prueba texto para ver si jala lo que estpy haciendo aqui no ocupo mucho texto xd balatro</p>
+                        </div>
+                        
+                    </div>
+                  <div class="FoodBtns" style="display:flex">
+                        <button>Ordenar</button>
+                            <button class="Mas">+</button>
+                                <p>Agregar</p>
+                            <button class="Menos">-</button>
+                    </div>
+                </div>
+                <div class="Item" style="display: grid;  grid-template-rows:100px,20px;" >
+                    <div class="FoodCard" style="display: flex;">
+                        <div class="FoodImage"style="width:150px; height:150px;  ">
+                            <img src="./Images/placeholder.png" style="width:100%; height:100%; object-fit: cover;">
+                        </div>
+                        <div class="FoodDescription">
+                            <h2>Wario en salsa verde</h2>
+                            <p><i>$25.80</i></p>
+                            <p>Item de comida blablbabla prueba texto para ver si jala lo que estpy haciendo aqui no ocupo mucho texto xd balatro</p>
+                        </div>
+                        
+                    </div>
+                   <div class="FoodBtns" style="display:flex">
+                        <button>Ordenar</button>
+                            <button class="Mas">+</button>
+                                <p>Agregar</p>
+                            <button class="Menos">-</button>
+                    </div>
+                </div>
+                  
+            </div>
+       </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/a8d9f3784b.js" crossorigin="anonymous"></script>
+    </body>
+</html>
